4f4063a6edd6056b26853d3dd243bfa3917c4e5d
Habeeb Ahmed <h37ahmed@uwaterloo.ca>
2015-12-23 22:35:40 -0500

    added in bouncy buttons

--- src/reboundbuttons.js
+++ src/reboundbuttons.js
@@ -2,81 +2,75 @@
 var BUTTON_HOVER_PRESS_DEPTH = 0.5;
 var BUTTON_CLICK_PRESS_DEPTH = 1;
 
+var buttonsToSprings = new Map();
+var springsToButtons = new Map();
+
 // Get a reference to the logo element.
-var github_btn = document.getElementById('github_btn');
-var linkedin_btn = document.getElementById('linkedin_btn');
+var buttons = [document.getElementById('github_btn') ,
+               document.getElementById('linkedin_btn'),
+               document.getElementById('throwned_icon'),
+               document.getElementById('music_player_icon'),
+               document.getElementById('wakey_wakey_icon'),
+               document.getElementById('peppit_icon')];
 
 // create a SpringSystem and a Spring with a bouncy config.
 var springSystem = new rebound.SpringSystem();
-var githubSpring = springSystem.createSpring(50, 3);
-var linkedinSpring = springSystem.createSpring(50, 3);
+var springs = [];
+
+for (i = 0 ; i < buttons.length ; i++) {
+  springs.push(springSystem.createSpring(50, 3));
+  buttonsToSprings.set(buttons[i].id, springs[i]);
+  springsToButtons.set(springs[i], buttons[i]);
+}
 
 // Add a listener to the spring. Every time the physics
 // solver updates the Spring's value onSpringUpdate will
 // be called.
-githubSpring.addListener({
-  onSpringUpdate: function(spring) {
-    var val = spring.getCurrentValue();
-    val = rebound.MathUtil.mapValueInRange(val, 0, 1, 1, 0.5);
-    scale(github_btn, val);
-  }
-});
+for (i = 0 ; i < springs.length ; i++) {
+  springs[i].addListener({
+    onSpringUpdate: function(spring) {
+      var val = spring.getCurrentValue();
+      val = rebound.MathUtil.mapValueInRange(val, 0, 1, 1, 0.5);
+      scale(springsToButtons.get(spring), val);
+    }
+  });
+}
 
-linkedinSpring.addListener({
-  onSpringUpdate: function(spring) {
-    var val = spring.getCurrentValue();
-    val = rebound.MathUtil.mapValueInRange(val, 0, 1, 1, 0.5);
-    scale(linkedin_btn, val);
-  }
-});
+// add spring inputs
+for (i = 0 ; i < buttons.length ; i++) {
+  buttons[i].addEventListener("mouseover", function(event) {
+    hover(buttonsToSprings.get(event.target.id));
+  }, false);
+}
 
-// github button springs
-github_btn.addEventListener("mouseover", function() {
-  hover(githubSpring);
-}, false);
+for (i = 0 ; i < buttons.length ; i++) {
+  buttons[i].addEventListener("mousedown", function(event) {
+    clicked(buttonsToSprings.get(event.target.id));
+  }, false);
+}
 
-github_btn.addEventListener('mousedown', function() {
-  clicked(githubSpring);
-});
+for (i = 0 ; i < buttons.length ; i++) {
+  buttons[i].addEventListener("mouseout", function(event) {
+    reset(buttonsToSprings.get(event.target.id));
+  }, false);
+}
 
-github_btn.addEventListener('mouseout', function() {
-  reset(githubSpring);
-});
-
-github_btn.addEventListener('mouseup', function() {
-  reset(githubSpring);
-});
-
-// linkedin button springs
-linkedin_btn.addEventListener("mouseover", function() {
-  hover(linkedinSpring);
-}, false);
-
-linkedin_btn.addEventListener('mousedown', function() {
-  clicked(linkedinSpring);
-});
-
-linkedin_btn.addEventListener('mouseout', function() {
-  reset(linkedinSpring);
-});
-
-linkedin_btn.addEventListener('mouseup', function() {
-  reset(linkedinSpring);
-});
+for (i = 0 ; i < buttons.length ; i++) {
+  buttons[i].addEventListener("mouseup", function(event) {
+    reset(buttonsToSprings.get(event.target.id));
+  }, false);
+}
 
 // button spring helper functions
 function hover(spring) {
-  console.log("hover");
   spring.setEndValue(BUTTON_HOVER_PRESS_DEPTH);
 }
 
 function reset(spring) {
-  console.log("reset");
   spring.setEndValue(0);
 }
 
 function clicked(spring) {
-  console.log("clicked");
   spring.setEndValue(BUTTON_CLICK_PRESS_DEPTH);
 }
 
